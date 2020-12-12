8c2ffded325e11e3a3417651be67201777357cfc
Desmond Aldridge <bob@hyainc.com>
2020-12-12 13:44:29 -0600

    Added the AJAX call to Google to search books

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
 export default {
+
+  // Get request to Google!
+  searchGoogleBooks: function(query) {
+    return axios.get("https://googelapis.com/v1/volumes?q=" + query)
+  },
+
   // Gets all books
   getBooks: function() {
     return axios.get("/api/books");
